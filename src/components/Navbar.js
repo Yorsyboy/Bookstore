07384a527ff67048680c45a87ebcdb6e40cf350d
yorsyboy <adekunletaiwo43@yahoo.com>
2022-06-09 00:07:06 +0100

    add redux thunk and api calls

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,17 @@ import { ImUser } from 'react-icons/im';
 
 const Navbar = () => (
   <nav className="navigation-bar">
-    <h1 className="title">Bookstore CMS</h1>
-    <ul className="navigation-links">
-      <li className="active">
-        <Link to="/">BOOKS</Link>
-      </li>
-      <li>
-        <Link to="/categories">CATEGORIES</Link>
-      </li>
-    </ul>
+    <div className="nav-item">
+      <h1 className="title">Bookstore CMS</h1>
+      <ul className="navigation-links">
+        <li className="active">
+          <Link to="/">BOOKS</Link>
+        </li>
+        <li>
+          <Link to="/categories">CATEGORIES</Link>
+        </li>
+      </ul>
+    </div>
     <ImUser />
   </nav>
 );
